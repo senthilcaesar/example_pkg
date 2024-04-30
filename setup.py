d4735c4aefe14d9d7d6329208d5084c9001dc75f
Senthil Palanivelu <spalanivelu@mgh.harvard.edu>
2024-04-30 13:35:54 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='example_pkg',
+    version='0.0.1',
+    author='Senthil Palanivelu',
+    author_email='senthilcaesar@gmail.com',
+    description='A simple example package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
+
